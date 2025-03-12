3fff2c9c35c178735709b085039ed415a84f1d9d
fahmihasanasagaf <sayafahmi18@gmail.com>
2025-03-13 00:48:34 +0700

    Menambahkan jurnalmodul4 ke dalam folder 04_Automata_dan_Table-Driven_Construction

---
+++ 04_Automata_dan_Table-Driven_Construction/jurnalmodul4/jurnalmodul4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace jurnalmodul4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosisiKarakterGame karakter = new PosisiKarakterGame();
+
+            while (true)
+            {
+                Console.Write("Tekan tombol (W/S/X, Q untuk keluar): ");
+                char input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (input == 'q' || input == 'Q')
+                {
+                    Console.WriteLine("Game selesai.");
+                    break;
+                }
+                else if (input == 'w' || input == 'W')
+                {
+                    karakter.TombolW();
+                }
+                else if (input == 's' || input == 'S')
+                {
+                    karakter.TombolS();
+                }
+                else if (input == 'x' || input == 'X')
+                {
+                    karakter.TombolX();
+                }
+            }
+        }
+    }
+}
