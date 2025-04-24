09254ac9e5fb2f246b63958e8feb57e8ad26b6f6
fahmihasanasagaf <sayafahmi18@gmail.com>
2025-04-24 22:36:19 +0700

    Menambahkan code SayaTubeVideo Dan Design Contract

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP_Modul6/TP_Modul6/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Fahmi hasan asagaf");
+
+        for (int i = 0; i < 100000; i++)
+        {
+            video.IncreasePlayCount(100000); // Uji overflow
+        }
+
+        video.PrintVideoDetails();
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP_Modul6/TP_Modul6/SayaVideoTube.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+            throw new ArgumentException("Title is invalid!");
+
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count > 10000000)
+            throw new ArgumentException("Play count increment exceeds limit.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
