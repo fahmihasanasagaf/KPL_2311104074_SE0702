352b6cacb49f1d7c67df1bc6cc56e43cb679fffc
fahmihasanasagaf <sayafahmi18@gmail.com>
2025-05-31 00:40:28 +0700

    Menambahkan code dan laporan jurnal 10

---
+++ 10_Library_Construction/JURNAL 10/MatematikaConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("FPB dari 60 dan 45 adalah: " + Matematika.FPB(60, 45));
+        Console.WriteLine("KPK dari 12 dan 8 adalah: " + Matematika.KPK(12, 8));
+
+        int[] persamaanTurunan = { 1, 4, -12, 9 };
+        Console.WriteLine("Turunan dari x^3 + 4x^2 - 12x + 9 adalah: ");
+        Console.WriteLine(Matematika.Turunan(persamaanTurunan));
+
+        int[] persamaanIntegral = { 4, 6, -12, 9 };
+        Console.WriteLine("Integral dari 4x^3 + 6x^2 -12x + 9 adalah: ");
+        Console.WriteLine(Matematika.Integral(persamaanIntegral));
+    }
+}
---
+++ 10_Library_Construction/JURNAL 10/MatematikaLibraries/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public static int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a);
+        }
+
+        public static int KPK(int a, int b)
+        {
+            return Math.Abs(a * b) / FPB(a, b);
+        }
+
+        public static string Turunan(int[] persamaan)
+        {
+            string hasil = "";
+            int pangkat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koef = persamaan[i] * pangkat;
+                if (koef == 0) continue;
+
+                if (hasil != "" && koef > 0)
+                    hasil += " + ";
+
+                if (koef < 0)
+                    hasil += " - " + Math.Abs(koef);
+                else
+                    hasil += koef;
+
+                if (pangkat - 1 > 0)
+                    hasil += "x" + (pangkat - 1);
+
+                pangkat--;
+            }
+
+            return hasil;
+        }
+
+        public static string Integral(int[] persamaan)
+        {
+            string hasil = "";
+            int pangkat = persamaan.Length;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                double koef = (double)persamaan[i] / (pangkat - i);
+                string strKoef = koef == 1 ? "" : koef == -1 ? "-" : koef.ToString();
+
+                if (hasil != "" && koef > 0)
+                    hasil += " + ";
+
+                if (koef < 0 && hasil == "")
+                    hasil += "-";
+
+                hasil += strKoef + "x" + (pangkat - i);
+            }
+
+            hasil += " + C";
+            return hasil;
+        }
+    }
+}
