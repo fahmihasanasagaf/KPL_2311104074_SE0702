15f66d00131682410d0558f81461e5d4b2e00f81
fahmihasanasagaf <sayafahmi18@gmail.com>
2025-05-31 00:29:27 +0700

    menambahkan code dan laporan TP 10

---
+++ 10_Library_Construction/TPmodul10/AljabarConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] kuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine("Hasil Kuadrat:");
+        foreach (var item in kuadrat)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.WriteLine("\n");
+
+        double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine("Akar Persamaan Kuadrat:");
+        foreach (var item in akar)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
